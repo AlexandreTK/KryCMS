d28cf7c0544ece017879a4b3db21f7d57fe3c726
Alexandre TK <alexandrekry@gmail.com>
2016-05-29 23:51:28 -0500

    adding admin field to user

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,25 @@
 class AdminController < ApplicationController
+ 
   before_filter :authenticate_user!
+  before_filter :ensure_admin
+
+
+private
+def ensure_admin
+  unless current_user && current_user.admin?
+    #render :text => "You are not authorised to perform this action", :status => :unauthorized
+    reset_session
+  	redirect_to user_session
+  end
 end
+
+end
+
+
+#before_filter :ensure_admin, :only => [:new, :create]
+#private
+#def ensure_admin
+# unless current_user && current_user.admin?
+#   render :text => "You are not authorised to perform this action", :status => :unauthorized
+# end
+#end
